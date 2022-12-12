5f2db69cc64b820ac430763c19e5bef4ed1a1717
itsOtto <luhatuman@yandex.ru>
2022-12-13 00:13:52 +0900

    Strings HW Completed

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        String phone = "+925 725-777-9"; // Инициализация строки
-        phone = phone.replace("-", ""); // МЕТОД replace (замена или удаление символа из строки)
-        phone = phone.replace(" ", "");
-        phone = phone.replace("+", "");
-        if (phone.length() == 10) { // МЕТОД lenght - длина строки
-            phone = '7' + phone;
-        } else if (phone.length() > 11) {
-            throw new RuntimeException("Телефон слишком длинный"); // Устойчивая конструкция, вывод сообщения об ошибке
-        } else if (phone.length() < 10) {
-            throw new RuntimeException("Телефон слишком короткий");
-        } else if (phone.length() == 11 && phone.charAt(0) != '7') { // МЕТОД charAt - чтение символа из строки
-            throw new RuntimeException("Среди нас посторонний");
-        }
-        System.out.println("phone = " + phone);
-        String expectedPhone = "79257257779";
-        if (phone.equals(expectedPhone)) { // МЕТОД equals - сравнение строк
-            System.out.println("Успех!");
-        } else {
-            System.out.println("Неудача!");
-        }
-        System.out.println(phone.charAt(10)); //распечатка 10-го символа из строки
+        //Task 1
+        System.out.println("Task 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        //Task 2
+        System.out.println("Task 2");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase()); // МЕТОД toUpperCase - меняющий регистр строки на верхний
+        //Task 3
+        System.out.println("Task 3");
+        String fullName1 = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника — " + fullName1.replace('ё', 'е'));
     }
 }
